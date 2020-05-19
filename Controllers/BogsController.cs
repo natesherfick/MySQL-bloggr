9523f3f41acd5ca829c7c40134c2e9add0f39976
Nate Sherfick <natesherfick@gmail.com>
2020-05-18 23:57:36 -0600

    setup

---
+++ Controllers/BogsController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MySQL_bloggr.Controllers
+{
+  [ApiController]
+  [Route("[api/controller]")]
+  public class BlogsController : ControllerBase
+  {
+    public ActionResult<string> GetAll()
+    {
+      try
+      {
+        return Ok("got blogs");
+      }
+      catch (System.Exception)
+      {
+
+        throw;
+      }
+    }
+  }
+}
